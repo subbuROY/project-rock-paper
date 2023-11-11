aeaf3d5788c8a820b1008aa1846e41906fe81239
subrahmanyam bassa <88094123+subbuROY@users.noreply.github.com>
2023-11-11 18:06:49 +0530

    Add files via upload

---
+++ Guess_game_by_two_users.py
@@ -0,0 +1,6 @@
+User_number1 = int(input("Enter Player1 Value:->"))
+User_number2 = int(input("Enter Player2 Value:->"))
+if User_number1 == User_number2:
+    print("Player2 Wins \nKeep Going Player1")
+else:
+    print("Player1 Wins \nKeep Going Player2")
